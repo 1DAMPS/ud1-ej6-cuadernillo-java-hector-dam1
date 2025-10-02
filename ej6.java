7e8015b3f2eaea031cf3794584f95eecdf2b9df2
hector-dam1 <hector.nunez.lopez.al@iespoligonosur.org>
2025-10-02 17:13:05 +0000

    Subida ej6.

---
+++ ej6.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ej6 {
+    public static void main(String[] args){
+        String variablePrimera = "A";
+        String variableSegunda = "B";
+        String variableTercera = "C";
+        System.out.println(variablePrimera + variableSegunda + variableTercera);
+    }
+}
